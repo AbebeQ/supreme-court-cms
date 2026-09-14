1577819922fbda4f6afcc0d36f0fb2a007c0fec8
AbebeQ <bihonegnabebe9@gmail.com>
2026-09-14 15:15:36 +0300

    Add .gitignore and remove config/uploads from staging

--- config/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-
-$pdo = new PDO("pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
